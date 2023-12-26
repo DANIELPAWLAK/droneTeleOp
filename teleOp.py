efd310c4dc89d0f0857f87cb0b5188cc1f088cba
DANIELPAWLAK <66480754+DANIELPAWLAK@users.noreply.github.com>
2023-12-26 17:12:52 +0000

    fixed input not being sent

--- teleOp.py
+++ teleOp.py
@@ -3,6 +3,7 @@ import cv2
 import pygame
 import sys
 
+
 class teleOp:
 
     def __init__(self):
@@ -10,7 +11,7 @@ class teleOp:
         self.Drone = tello.Tello()
         self.Drone.connect()
 
-        self.pGame = pygame.init()
+        pygame.init()
         self.speed = 80
         self.liftSpeed = 80
         self.moveSpeed = 50
@@ -19,47 +20,54 @@ class teleOp:
         self.operateDrone()
 
     def keyboardInput(self):
-        #Array with drone values left/right,front/back,up/down,yaw velocity
-        droneVals = [0,0,0,0]
-        key = pygame.key.get_pressed() #Gets key being pressed
-        #Checks if app is closed to shutoff program
+        # Array with drone values left/right,front/back,up/down,yaw velocity
+        droneVals = [0, 0, 0, 0]
+        key = pygame.key.get_pressed()  # Gets key being pressed
+        # Checks if app is closed to shutoff program
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        #Right/Left
-        if key[pygame.K_RIGHT]: droneVals[0] = self.speed
-        elif key[pygame.K_LEFT]: droneVals[0] = -self.speed
-        #Forwards/Backwards
-        if key[pygame.K_UP]: droneVals[1] = self.moveSpeed
-        elif key[pygame.K_DOWN]: droneVals[1] = -self.moveSpeed
-        #Up/Down
-        if key[pygame.K_w]: droneVals[2] = self.liftSpeed
-        elif key[pygame.K_s]: droneVals[2] = -self.liftSpeed
-        #Rotate right/left
-        if key[pygame.K_d]: droneVals[3] = self.rotationSpeed
-        elif key[pygame.K_a]: droneVals[3] = -self.rotationSpeed
-        #Liftoff/Land
-        if key[pygame.K_SPACE]: print("liftoff")
-        elif key[pygame.K_q]: print("land")
+        # Right/Left
+        if key[pygame.K_RIGHT]:
+            droneVals[0] = self.speed
+        elif key[pygame.K_LEFT]:
+            droneVals[0] = -self.speed
+        # Forwards/Backwards
+        if key[pygame.K_UP]:
+            droneVals[1] = self.moveSpeed
+        elif key[pygame.K_DOWN]:
+            droneVals[1] = -self.moveSpeed
+        # Up/Down
+        if key[pygame.K_w]:
+            droneVals[2] = self.liftSpeed
+        elif key[pygame.K_s]:
+            droneVals[2] = -self.liftSpeed
+        # Rotate right/left
+        if key[pygame.K_d]:
+            droneVals[3] = self.rotationSpeed
+        elif key[pygame.K_a]:
+            droneVals[3] = -self.rotationSpeed
+        # Liftoff/Land
+        if key[pygame.K_SPACE]:
+            self.Drone.takeoff()
+        elif key[pygame.K_q]:
+            self.Drone.land()
 
         return droneVals
 
     def operateDrone(self):
-        windows = pygame.display.set_mode((400, 400))
+        print(self.Drone.get_battery())
+
         self.Drone.streamon()
         while True:
-
             keyVals = self.keyboardInput()
-            self.Drone.send_rc_control(keyVals)
+            self.Drone.send_rc_control(keyVals[0], keyVals[1], keyVals[2], keyVals[3])
 
-            img = cv2.resize(self.Drone.get_frame_read(), (1080,720))
+            img = self.Drone.get_frame_read().frame
             cv2.imshow('Drone Feed', img)
             cv2.waitKey(1)
 
+
 if __name__ == "__main__":
     teleOp()
-
-
-
-
